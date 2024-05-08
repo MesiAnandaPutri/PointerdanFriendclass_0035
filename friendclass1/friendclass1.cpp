c4dabcd529cd16a3dffa532879befd7f7dd142e5
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-05-08 15:15:09 +0700

    class orang

---
+++ friendclass1/friendclass1.cpp
@@ -0,0 +1,17 @@
+// friendclass1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
